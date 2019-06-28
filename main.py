b9540ff707f6a1725edd411aeecf843b8da03029
Deltoidex <rogerbm26@gmail.com>
2019-06-28 16:19:56 +0200

    First filter updated and saved data types with correct format

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def read():
     return df
 
 
-def check_data(csv):
+def check_missing_data(csv):
 
+    # Checking is has any missing values
     if not (csv.isnull().values.any()):
         print("|-----------------|")
         print("|  Data is clean  |")
@@ -29,6 +30,8 @@ def list_of_headers(csv):
 
 
 def first_filter_of_data(csv):
+
+    # listing all data types from pandas library
     first_data_types = csv.dtypes.tolist()
 
     return first_data_types
@@ -40,6 +43,7 @@ def second_filter_of_data(csv):
     return second_data_types
 
 
+
 def print_data(headers, filter):
     for x, y in zip(headers, filter):
         # print("Header:    " + x + "      has value type:    " + str(y))
@@ -48,26 +52,39 @@ def print_data(headers, filter):
 
 if __name__ == "__main__":
     csv = read()
-    check_data(csv)
+    check_missing_data(csv)
 
     # Take a look at the first few rows
     # print(csv.head())
     # print(" ")
 
-    headers = list_of_headers(csv)
+    list_of_headers = list_of_headers(csv)  # list of all headers
     print("")
-    print("Hearders of the dataset: "+str(headers))
-    # print(type(headers))
+    print("Hearders of the dataset: "+str(list_of_headers))
 
-    first_filter = first_filter_of_data(csv)
+    first_filter = first_filter_of_data(csv)  # list of types of data
+
+    # first data types cleaned in format
+    first_data_types = []
+    for type_data in first_filter:
+        first_data_types.append(str(type_data))
+        #print(type_data,type(type_data))
+    #print(type(first_data_types[0]))
+
+
     print("")
     print("Here we have the first filter of data:")
-    print_data(headers, first_filter)
+    print_data(list_of_headers, first_data_types)
 
+    print("First filter list: ", first_data_types)
+
+    """
     second_filter = second_filter_of_data(csv)
     print("")
-    print("Here we have the first filter of data:")
-    print_data(headers, second_filter)
+    print("Here we have the second filter of data:")
+    print_data(list_of_headers, second_filter)
+    """
+
 
     print(" ")
     # print(csv['Genres'])
